4568a9b6fa2c4c5bd7d41c930413260bdd125df6
lisknonanika <lisknonanika@gmail.com>
2021-09-22 17:05:57 +0900

    upd

--- src/app/common/lisk-utils.ts
+++ src/app/common/lisk-utils.ts
@@ -69,7 +69,7 @@ export const getSendTransaction = async(network:number, publicKey:string, nonce:
 
 export const getTransactions = async(network:number, address:string):Promise<any[]> => {
   try {
-    const res = await fetch(`${getApiURL(network)}/v2/transactions?address=${address}&offset=0&limit=10`);
+    const res = await fetch(`${getApiURL(network)}/v2/transactions?address=${address}&offset=0&limit=5`);
     const json = await res.json();
     return json.data.error? null: json.data;
   } catch (err) {
